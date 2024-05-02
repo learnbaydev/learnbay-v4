1af5e1df83572abce0d4221ba108c72c8cbf4131
Amit A <amita@Amits-MacBook-Pro.local>
2024-05-02 11:05:03 +0530

    global color heromsection

---
+++ src/pages/data-analytics.js
@@ -0,0 +1,13 @@
+import Hero from '@/components/course/hero/hero';
+import React from 'react'
+
+const DataAnalytics = () => {
+  return (
+    <div>
+      <h2>Data Analytics</h2>
+      <Hero/>
+    </div>
+  )
+}
+
+export default DataAnalytics;
