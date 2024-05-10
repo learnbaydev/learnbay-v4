6e3303a1d5387fee8d62081943ec0fa262ac420a
Amit A <amita@Amits-MacBook-Pro.local>
2024-05-10 12:03:58 +0530

    Canvas set

--- src/components/course/domainElective/domainData.js
+++ src/components/course/domainElective/domainData.js
@@ -1,28 +1,36 @@
 const domainData = [
-    {
-      name: "Healthcare",
-      imageUrl: "https://d32and0ii3b8oy.cloudfront.net/web/V4/Coursepage/domain-icon-one.webp",
-      hoverImageUrl: "https://d32and0ii3b8oy.cloudfront.net/web/V4/Coursepage/domain-icon-green-1.webp",
-      description: "For retail, e-commerce & supply." 
-    },
-    {
-      name: "Supply Chain",
-      imageUrl: "https://d32and0ii3b8oy.cloudfront.net/web/V4/Coursepage/domain-icon-two.webp",
-      hoverImageUrl: "https://d32and0ii3b8oy.cloudfront.net/web/V4/Coursepage/domain-icon-green-2.webp",
-      description: "For retail, e-commerce & supply."
-    },
-    {
-      name: "Manufacturing",
-      imageUrl: "https://d32and0ii3b8oy.cloudfront.net/web/V4/Coursepage/domain-icon-three.webp",
-      hoverImageUrl: "https://d32and0ii3b8oy.cloudfront.net/web/V4/Coursepage/domain-icon-green-3.webp",
-      description: "For retail, e-commerce & supply"
-    },
-    {
-      name: "Banking & Finance",
-      imageUrl: "https://d32and0ii3b8oy.cloudfront.net/web/V4/Coursepage/domain-icon-four.webp",
-      hoverImageUrl: "https://d32and0ii3b8oy.cloudfront.net/web/V4/Coursepage/domain-icon-green-4.webp",
-      description: "For retail, e-commerce & supply.."
-    }
-  ];
-  
-  export default domainData;
+  {
+    name: "Healthcare",
+    imageUrl:
+      "https://d32and0ii3b8oy.cloudfront.net/web/V4/Coursepage/domain-icon-one.webp",
+    hoverImageUrl:
+      "https://d32and0ii3b8oy.cloudfront.net/web/V4/Coursepage/domain-icon-green-1.webp",
+    description: "For retail, e-commerce & supply.",
+  },
+  {
+    name: "Supply Chain",
+    imageUrl:
+      "https://d32and0ii3b8oy.cloudfront.net/web/V4/Coursepage/domain-icon-two.webp",
+    hoverImageUrl:
+      "https://d32and0ii3b8oy.cloudfront.net/web/V4/Coursepage/domain-icon-green-2.webp",
+    description: "For retail, e-commerce & supply.",
+  },
+  {
+    name: "Manufacturing",
+    imageUrl:
+      "https://d32and0ii3b8oy.cloudfront.net/web/V4/Coursepage/domain-icon-three.webp",
+    hoverImageUrl:
+      "https://d32and0ii3b8oy.cloudfront.net/web/V4/Coursepage/domain-icon-green-3.webp",
+    description: "For retail, e-commerce & supply",
+  },
+  {
+    name: "Banking & Finance",
+    imageUrl:
+      "https://d32and0ii3b8oy.cloudfront.net/web/V4/Coursepage/domain-icon-four.webp",
+    hoverImageUrl:
+      "https://d32and0ii3b8oy.cloudfront.net/web/V4/Coursepage/domain-icon-green-4.webp",
+    description: "For retail, e-commerce & supply..",
+  },
+];
+
+export default domainData;
